41369555a2cefbb96055cd2d56152305bb534011
camgelozzi <cangelozzi@gmail.com>
2019-08-30 23:25:04 +0200

    initial component structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+
 import './App.css';
 
 class App extends Component {
@@ -8,19 +10,40 @@ class App extends Component {
         super();
 
         this.state = {
-            string: "Hello Camillo from React"
+            monsters: [],
+            searchField: ''
         };
+
+        //! define this context for methods created by me
+        // TODO Use ()=>{} arrow functions on any class methods you define and are not part of React ie.render(), componentDidMount()...
+        // this.handleChange = this.handleChange.bind(this);
 
     }
 
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(users => this.setState({monsters: users}));
+    }
+
+    //! arrow function BIND the this to the class, when the class is created, avoiding the .bind()
+    // TODO Use ()=>{} arrow functions on any class methods you define and are not part of React ie.render(), componentDidMount()...
+    handleChange = e => {
+        this.setState({ searchField: e.target.value })
+    }
+
     render() {
+
+        // show filtered monsters
+        const { monsters, searchField } = this.state;
+        const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
+
         return(
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>{ this.state.string }</p>
-                    <button onClick={() => this.setState({ string: 'Hello World' })}>Change Text</button>
-                </header>
+                <SearchBox 
+                    placeholder="search for robo-monster" 
+                    handleChange={this.handleChange} />
+                <CardList monsters={filteredMonsters} />
             </div>
         )
     }
